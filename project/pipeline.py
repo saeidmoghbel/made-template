17d7055311cd356065f2a77b4f04fe0a39d401ad
saeid moghbel <saeed.moghbel@yahoo.com>
2024-01-09 22:23:14 +0100

    pipeline improved

--- project/pipeline.py
+++ project/pipeline.py
@@ -8,9 +8,7 @@ import matplotlib.pyplot as plt
 
 
 
-url_2009 = "https://datamillnorth.org/download/road-traffic-accidents/288d2de3-0227-4ff0-b537-2546b712cf00/2009.csv"
-url_2015 = "https://datamillnorth.org/download/road-traffic-accidents/df98a6dd-704e-46a9-9d6d-39d608987cdf/2015.csv"
-url_2016 = "https://datamillnorth.org/download/road-traffic-accidents/b2c7ebba-312a-4b3d-a324-6a5eda85fa5b/Copy%2520of%2520Leeds_RTC_2016.csv"
+
 
 def fetch_and_read(url):
     """
@@ -24,7 +22,8 @@ def fetch_and_read(url):
     """
     try:
         response = requests.get(url)
-        return pd.read_csv(io.StringIO(response.text), sep=";", error_bad_lines='skip')
+        print(io.StringIO(response.text))
+        return pd.read_csv(io.StringIO(response.text), sep=",", on_bad_lines='skip')
     except requests.RequestException as e:
         print(f"Error fetching data from {url}: {e}")
         return pd.DataFrame()
@@ -32,7 +31,7 @@ def fetch_and_read(url):
         print(f"Error parsing CSV from {url}: {e}")
         return pd.DataFrame()
     
-def clean_datasets(cleaned_df):
+def clean_datasets(combined_df):
     """
     Cleans and analyzes datasets.
 
@@ -42,10 +41,7 @@ def clean_datasets(cleaned_df):
     Returns:
         pandas.DataFrame: The cleaned dataset.
     """
-    df_1 = fetch_and_read(url_2009)
-    df_2 = fetch_and_read(url_2015)
-    df_3 = fetch_and_read(url_2016)
-    combined_df = pd.concat([df_1, df_2, df_3], ignore_index=True)
+    
     selected_columns = combined_df.loc[:, ["Casualty Severity", "Type of Vehicle"]]
     cleaned_df = clean_data(selected_columns)
     analyze_data(cleaned_df)
@@ -66,9 +62,9 @@ def clean_data(df):
     df.dropna(subset=["Type of Vehicle", "Casualty Severity"], inplace=True)
     df["Type of Vehicle"] = df["Type of Vehicle"].str.lower()
     df["Casualty Severity"] = df["Casualty Severity"].str.lower()
-    df["Type of Vehicle"] = df["Type of Vehicle"].replace({"Taxi/Private hire car": "Car", "M/cycle 50cc and under": "Pedal cycle", "Other Vehicle": "Car",
-                                                           "Motorcycle over 125cc and up to 500cc": "Pedal cycle", "Goods vehicle 3.5 tonnes mgw and under": "Bus or coach (17 or more passenger seats)"})
-    severity_order = ["Slight", "Serious", "Fatal"]
+    df["Type of Vehicle"] = df["Type of Vehicle"].replace({"Taxi/Private hire car": "car_2", "M/cycle 50cc and under": "cycle", "Other Vehicle": "car_1",
+                                                           "Motorcycle over 125cc and up to 500cc": "cycle", "Goods vehicle 3.5 tonnes mgw and under": "bus"})
+    severity_order = ["slight", "serious", "fatal"]
     df["Casualty Severity"] = pd.Categorical(df["Casualty Severity"], categories=severity_order, ordered=True)
     return df
 
@@ -83,20 +79,21 @@ def analyze_data(cleaned_df):
     None
     """
     selected_df = cleaned_df.loc[:, ["Casualty Severity", "Type of Vehicle"]]
-    x=sns.countplot(x="Type of Vehicle", hue="Casualty Severity", data=selected_df)
+    #chart_1 = sns.countplot(x="Type of Vehicle", hue="Casualty Severity", data=selected_df)
+    chart_2=sns.histplot(data=selected_df, x="Type of Vehicle", hue="Casualty Severity")
+    chart_2.set_xticklabels(chart_2.get_xticklabels(), rotation=45, horizontalalignment='right')
     plt.title("Count of Casualty Severity for each Type of Vehicle")
-    plt.plot(x)
     plt.show()
 
 # Call the analyze_data function with the cleaned_df DataFrame
-    analyze_data(cleaned_df)
+
     
 def load_data(df, db_path, accidents, cleaned_df):
     """
     Load the given DataFrame into a SQLite database.
 
     Parameters:
-    df (pandas.DataFrame): The DataFrame to be loaded into the database.
+    (pandas.DataFrame): The DataFrame to be loaded into the database.
     db_path (str): The path to the SQLite database file.
     accidents (str): The name of the table to be created in the database.
     cleaned_df (pandas.DataFrame): The cleaned DataFrame.
@@ -108,3 +105,18 @@ def load_data(df, db_path, accidents, cleaned_df):
     df.to_sql(accidents, conn, index=False, if_exists='replace')
     conn.commit()
     conn.close()
+    
+    
+def main():
+    url_2009 = "https://datamillnorth.org/download/road-traffic-accidents/288d2de3-0227-4ff0-b537-2546b712cf00/2009.csv"
+    url_2015 = "https://datamillnorth.org/download/road-traffic-accidents/df98a6dd-704e-46a9-9d6d-39d608987cdf/2015.csv"
+    url_2016 = "https://datamillnorth.org/download/road-traffic-accidents/b2c7ebba-312a-4b3d-a324-6a5eda85fa5b/Copy%2520of%2520Leeds_RTC_2016.csv"
+    df_1 = fetch_and_read(url_2009)
+    df_2 = fetch_and_read(url_2015)
+    df_3 = fetch_and_read(url_2016)
+    combined_df = pd.concat([df_1, df_2, df_3], ignore_index=True)
+    cleaned_df = clean_datasets(combined_df)
+    analyze_data(cleaned_df)
+    
+if __name__ == '__main__':
+    main()
